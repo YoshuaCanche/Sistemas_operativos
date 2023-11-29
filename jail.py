f83b5558c48a556d803802969e800b660e3af697
YoshuaCanche <116574279+YoshuaCanche@users.noreply.github.com>
2023-11-29 00:50:44 -0500

    Add files via upload

---
+++ jail.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import code
+
+
+preguntas_y_respuestas = {
+    1: ("¿Qué comando se usa para ver el contenido de un archivo?", "cat"),
+    2: ("¿Cómo se crea un nuevo directorio?", "mkdir"),
+    3: ("¿Qué comando se utiliza para copiar archivos?", "cp")
+}
+
+def create_linux_terminal_trivia_jail():
+
+    jail_dir = '/path/to/jail'  
+    if not os.path.exists(jail_dir):
+        os.makedirs(jail_dir)
+
+    jail_globals = {
+        '__builtins__': {},
+        'os': {},
+        'sys': {},
+        'code': {},
+        'print': print,
+    }
+
+    jail_console = code.InteractiveConsole(locals=jail_globals)
+
+    while True:  
+        print("¡Bienvenido al juego de trivia de la terminal de Linux!") 
+        puntuacion = 0
+        intentos = 3
+
+        for pregunta_id, (pregunta, respuesta) in preguntas_y_respuestas.items():
+            if intentos > 0:
+                print(f"Pregunta {pregunta_id}: {pregunta}")
+                respuesta_usuario = input("Tu respuesta: ")
+
+                if respuesta_usuario.lower() == respuesta:
+                    print("¡Correcto!")
+                    puntuacion += 1
+                else:
+                    print("Incorrecto.")
+                
+                intentos -= 1
+
+        print(f"Tu puntuación final es: {puntuacion} de {len(preguntas_y_respuestas)}")
+
+        if puntuacion >= 2:
+            print("¡Felicidades! Has pasado al siguiente nivel.")
+            break 
+        else:
+            print("No has obtenido suficientes puntos para pasar al siguiente nivel. ¡Inténtalo de nuevo!")
+
+if __name__ == '__main__':
+    create_linux_terminal_trivia_jail()
